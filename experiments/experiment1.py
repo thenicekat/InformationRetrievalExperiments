9174f8b42274bb7bb683295bfe0e84dbb67645fb
Saphereye <adarshdas950@gmail.com>
2024-02-17 17:19:17 +0530

    feat: Added grep profiling for experiment1

--- experiments/experiment1.py
+++ experiments/experiment1.py
@@ -3,6 +3,87 @@ from profiling import *
 from main import *
 import json
 from tqdm import tqdm
+import subprocess
+
+
+def grep_boolean_retrieval():
+    with open("s2/s2_query.json") as f:
+        queries = [i["query"] for i in json.load(f)["queries"]]
+    
+    os.makedirs("experiments/experiment1_temp", exist_ok=True)
+
+    for q in tqdm(queries):
+        try:
+            doc_count = 0
+            for keyword in q.split(" "):
+                subprocess.run(
+                    f"grep '^{keyword}\s' s2/intermediate/postings.tsv > experiments/experiment1_temp/output{doc_count}.txt",
+                    shell=True,
+                    check=True,
+                    stdout=subprocess.PIPE,
+                )
+                doc_count += 1
+            subprocess.run(
+                f"cat {' '.join(f'experiments/experiment1_temp/output{i}.txt' for i in range(doc_count))} | grep -Eo '[[:alnum:]]+' | sort | uniq -c | sort -rn | sed 1q > experiments/experiment1_temp/common.txt",
+                shell=True,
+                check=True,
+                stdout=subprocess.PIPE,
+            )
+        except subprocess.CalledProcessError:
+            print(f"Error with query: {q}")
+
+def grep_boolean_retrieval_time():
+    with open("s2/s2_query.json") as f:
+        queries = [i["query"] for i in json.load(f)["queries"]]
+    
+    os.makedirs("experiments/experiment1_temp", exist_ok=True)
+
+    for q in queries:
+        try:
+            doc_count = 0
+            for keyword in q.split(" "):
+                subprocess.run(
+                    f"grep '^{keyword}\s' s2/intermediate/postings.tsv > experiments/experiment1_temp/output{doc_count}.txt",
+                    shell=True,
+                    check=True,
+                    stdout=subprocess.PIPE,
+                )
+                doc_count += 1
+            subprocess.run(
+                f"cat {' '.join(f'experiments/experiment1_temp/output{i}.txt' for i in range(doc_count))} | grep -Eo '[[:alnum:]]+' | sort | uniq -c | sort -rn | sed 1q > experiments/experiment1_temp/common.txt",
+                shell=True,
+                check=True,
+                stdout=subprocess.PIPE,
+            )
+        except subprocess.CalledProcessError:
+            print(f"Error with query: {q}")
+
+@memory_profile
+def grep_boolean_retrieval_memory():
+    with open("s2/s2_query.json") as f:
+        queries = [i["query"] for i in json.load(f)["queries"]]
+    
+    os.makedirs("experiments/experiment1_temp", exist_ok=True)
+
+    for q in queries:
+        try:
+            doc_count = 0
+            for keyword in q.split(" "):
+                subprocess.run(
+                    f"grep '^{keyword}\s' s2/intermediate/postings.tsv > experiments/experiment1_temp/output{doc_count}.txt",
+                    shell=True,
+                    check=True,
+                    stdout=subprocess.PIPE,
+                )
+                doc_count += 1
+            subprocess.run(
+                f"cat {' '.join(f'experiments/experiment1_temp/output{i}.txt' for i in range(doc_count))} | grep -Eo '[[:alnum:]]+' | sort | uniq -c | sort -rn | sed 1q > experiments/experiment1_temp/common.txt",
+                shell=True,
+                check=True,
+                stdout=subprocess.PIPE,
+            )
+        except subprocess.CalledProcessError:
+            print(f"Error with query: {q}")
 
 
 def boolean_retrieval():
@@ -11,7 +92,8 @@ def boolean_retrieval():
 
     for q in tqdm(queries):
         try:
-            and_query(q.split(" "), "s2/")
+            output = and_query(q.split(" "), "s2/")
+            print(output)
         except KeyError:
             print(f"Error with query: {q}")
 
@@ -40,5 +122,4 @@ def boolean_retrieval_time_profile():
 
 
 if __name__ == "__main__":
-    # time_profile.run('boolean_retrieval_time_profile()')
-    boolean_retrieval_memory_profile()
+    time_profile.run("grep_boolean_retrieval_time()")
