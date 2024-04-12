af38fc0a25335ca63b4500d06842262521f09ab2
Shivam16 <shivamtrivedia@gmail.com>
2024-04-12 19:32:22 +0530

    indexing run on given text files

---
+++ Assignment_2/lucene-tutorial/src/LuceneConstants.java
@@ -0,0 +1,20 @@
+public class LuceneConstants {
+   public static final String CONTENTS = "contents";
+   public static final String FILE_NAME = "filename";
+   public static final String FILE_PATH = "filepath";
+   public static final int MAX_SEARCH = 10;
+   public static final String ID = "id";
+   public static final String URL = "url";
+   public static final String TITLE = "title";
+   public static final String ABSTRACT = "abstract";
+   public static final String MAINTEXT = "maintext";
+   public static final String COMMENTS = "comments";
+   public static final String TOPICS_TAGS = "topics tags";
+   public static final String DESCRIPTION = "description";
+   public static final String DOCTORS_NOTE = "doctors note";
+   public static final String ARTICLE_LINKS = "article links";
+   public static final String QUESTION_LINKS = "question links";
+   public static final String TOPIC_LINKS = "topic links";
+   public static final String VIDEO_LINKS = "video links";
+   public static final String MEDARTICLE_LINKS = "medarticle links";
+}
