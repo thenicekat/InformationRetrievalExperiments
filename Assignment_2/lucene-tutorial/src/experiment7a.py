a87b158f8082f60606f47e72d13481aaea387adb
Divyateja Pasupuleti <divyateja2004@gmail.com>
2024-04-23 17:29:46 +0530

    fix: modify qrel to work with pandas

---
+++ Assignment_2/lucene-tutorial/src/experiment7a.py
@@ -0,0 +1,14 @@
+"""
+Learning to Rank models (Total 15, 3 for report).
+You should experiment with at least 1 model for pointwise, pairwise, and
+list wise approaches. You are free (but not restricted) to use the libraries
+referred here for your experiments
+"""
+
+# This is where we shall code our point wise ltr model
+# Basically a classification task
+
+import pandas as pd
+
+merged_qrel = pd.read_csv('../../nfcorpus/merged.qrel', sep="\t")
+print(merged_qrel.head());
