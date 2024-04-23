26b7f86710d9db4a8d769ce8f944ea2ae7a3d504
Divyateja Pasupuleti <divyateja2004@gmail.com>
2024-04-24 00:59:55 +0530

    chore: rename file to client

---
+++ Assignment_2/lucene-tutorial/src/client.py
@@ -0,0 +1,27 @@
+from py4j.java_gateway import JavaGateway 
+
+gateway = JavaGateway() 
+luceneServer = gateway.entry_point 
+
+n = luceneServer.numDocs("Assignment_2/index")
+print("Number of documents = " + str(n))
+
+df = luceneServer.getDocFreq("Assignment_2/index", "abstract", "deep")
+print("Doc Freq = " + str(df))
+
+cf = luceneServer.getCollectionFreq("Assignment_2/index", "abstract", "deep")
+print("Collection Freq = " + str(cf))
+
+x = 0
+for i in range(n):
+    tf = luceneServer.getTermFreq("Assignment_2/index", "title", "deep", 0)
+    if(tf != 0):
+        x += tf
+    print("Doc = " + str(i) + " Term Freq = " + str(tf))
+print(x)
+
+coln_len = luceneServer.getCollectionLen("Assignment_2/index", "abstract")
+print("Collection Length = " + str(coln_len))
+
+avg_len = luceneServer.getAvgDocLen("Assignment_2/index", "abstract")
+print("Average Document Length = " + str(avg_len))
