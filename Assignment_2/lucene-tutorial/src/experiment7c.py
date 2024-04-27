4b8d5d9de342fc59d55dd41e71c54b84cf61849d
Divyateja Pasupuleti <divyateja2004@gmail.com>
2024-04-27 16:46:10 +0530

    fix: update 7update 7c

--- Assignment_2/lucene-tutorial/src/experiment7c.py
+++ Assignment_2/lucene-tutorial/src/experiment7c.py
@@ -18,6 +18,7 @@ import tensorflow as tf
 import tensorflow_recommenders as tfrs
 import tensorflow_ranking as tfr
 from tqdm import tqdm
+import indexer
 
 
 logging.basicConfig(
@@ -33,6 +34,8 @@ BATCH_SIZE = 128
 # Read the merged qrel file and delete the column named 'ZERO'
 merged_qrel = pd.read_csv('../../nfcorpus/merged.qrel', sep="\t")
 del merged_qrel['ZERO']
+
+postings, doc_freq, doc_ids, query_term_id_mapping = indexer.load_index_in_memory('../../nfcorpus/raw/')
 
 # read queries
 queries_dev = pd.read_csv('../../nfcorpus/dev.titles.queries', sep="	", names=['QUERY_ID', 'QUERY'])
@@ -61,8 +64,7 @@ relevance_rank = relevance_rank.map(lambda x: {
     "relevance": x["RELEVANCE"],
 })
 
-# Get the unique users and restaurants to create the vocabularies
-
+# Get the unique queries and documents to create the vocabularies
 unique_query_ids = np.unique(np.concatenate(list(relevance_rank.batch(1_000).map(lambda x: x["query_id"]))))
 del documents
 logging.info("Unique query ids: %d" % len(unique_query_ids))
@@ -87,10 +89,11 @@ for example in relevance_rank:
 
 # Create a tensor slice for each query-document pair.
 # the slice here is a dict of query_id, document_id and relevance
-tensor_slices = {"query_id": [], "document_id": [], "relevance": []}
+tensor_slices = {"query": [], "document_id": [], "relevance": []}
 
 for query_id, feature_lists in tqdm(doc_lists_by_query.items()):
-    logging.info(f"Query ID: {query_id}")
+  try:
+    logging.info(f"Query ID: {query_id.decode('utf-8')}, Query: {queries[query_id.decode('utf-8')]}")
     
     for _ in range(num_list_per_query):
         # Drop the query if they don't have enough diocuments.
@@ -98,7 +101,7 @@ for query_id, feature_lists in tqdm(doc_lists_by_query.items()):
             continue
         
         # randomly choose 10 documents
-        tensor_slices["query_id"].append(query_id)
+        tensor_slices["query"].append(queries[query_id.decode('utf-8')])
         
         random_indices = np.random.choice(len(feature_lists["document_id"]), num_docs_per_list, replace=False)
         x = tf.stack(np.array(feature_lists["document_id"])[random_indices]) 
@@ -106,8 +109,10 @@ for query_id, feature_lists in tqdm(doc_lists_by_query.items()):
         tensor_slices["relevance"].append(tf.stack(np.array(feature_lists["relevance"], dtype=np.float32)[random_indices]))
         
         logging.info(f"Iteration {_}: Reduced to {len(x)} from {len(feature_lists['document_id'])}")
+  except Exception as e:
+    pass
 
-logging.info("Number of slices: %d" % len(tensor_slices["query_id"]))
+logging.info("Number of slices: %d" % len(tensor_slices["query"]))
 logging.info("Number of unique documents: %d" % len(document_id_vocab))
 
 buffer_size = len(relevance_rank)
@@ -124,15 +129,16 @@ class LTRModel(tfrs.Model):
     embedding_dimension = 256
 
     # Query embeddings
-    # self.query_embeddings = tf.keras.Sequential([
-    #   tf.keras.layers.StringLookup(
-    #     vocabulary=unique_query_ids),
-    #   tf.keras.layers.Embedding(len(unique_query_ids) + 2, embedding_dimension)
-    # ])
-    # convert query sentence to a vector instead of just lookup table
+    # We have a sentence per query
+    # convert sentence to embedding
+    # use lstm
     self.query_embeddings = tf.keras.Sequential([
-      tf.keras.layers.TextVectorization(max_tokens=20000),
-      tf.keras.layers.Embedding(20002, embedding_dimension)
+      tf.keras.layers.TextVectorization(
+        max_tokens=20000,
+        vocabulary=list(postings.keys()),
+      ),
+      tf.keras.layers.Embedding(20002, embedding_dimension),
+      tf.keras.layers.GlobalAveragePooling1D(),
     ])
 
     # Document embeddings
@@ -159,15 +165,22 @@ class LTRModel(tfrs.Model):
     )
 
   def call(self, features):
-    query_embeddings = self.query_embeddings(features["query_id"])
+    query_embeddings = self.query_embeddings(features["query"])
+    logging.info(f"Query embeddings: {query_embeddings.shape}")
     document_embeddings = self.document_embeddings(features["document_id"])
+    logging.info(f"Document embeddings: {document_embeddings.shape}")
 
     # The embeddings are concatenated
+    # we get the length of the list of documents
     list_length = features["document_id"].shape[1]
-    user_embedding_repeated = tf.repeat(
+    # repeat the query embedding to match the length of the list
+    query_embedding_repeated = tf.repeat(
         tf.expand_dims(query_embeddings, 1), [list_length], axis=1)
+    # reshape the query embedding to match the document embedding
+  
+    # concatenate the query embedding with the document embeddings
     concatenated_embeddings = tf.concat(
-        [user_embedding_repeated, document_embeddings], 2)
+        [query_embedding_repeated, document_embeddings], 2)
 
     return self.score_model(concatenated_embeddings)
 
@@ -183,11 +196,20 @@ cached_train = train.shuffle(1000).batch(8192).cache()
 
 listwise_model = LTRModel(tfr.keras.losses.ListMLELoss())
 listwise_model.compile(optimizer=tf.keras.optimizers.Adagrad(0.1))
-listwise_model.fit(cached_train, epochs=5, verbose=True)
+listwise_model.fit(cached_train, epochs=3, verbose=True)
 
-# Evaluate on test set
-cached_test = test.batch(8192).cache()
-print(listwise_model.evaluate(cached_test))
+# Evaluate on test query
+query = "What is the best way to cook a turkey"
+documents = np.random.choice(list(document_id_vocab), 10)
+test_features = {
+  "query": tf.constant([query]),
+  "document_id": tf.constant([documents]),
+}
+res = listwise_model(test_features)
+res = res.numpy().flatten()
+for i in range(10):
+    logging.info(f"Document: {documents[i]} | Predicted relevance: {res[i]}")
+
 
 # save the model
 listwise_model.save_weights('./ltr_models/7c')
